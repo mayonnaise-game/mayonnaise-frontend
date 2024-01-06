82f9b525ea8763375b1d586da8ef1484259d2a21
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-01-06 17:26:19 +0900

    init setting

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
+"use client";
+
+import { Title, LandingButton, Container } from "@/components/home/home.styles";
+import { TextField } from "@mui/material";
+
 export default function Home() {
-  return <main>
-    <div></div>
-  </main>;
+  return (
+    <main>
+      <Container>
+        <Title>괜찮은 게임 제목</Title>
+        <TextField placeholder="이름을 입력하세요" />
+        <LandingButton variant="contained">게임 시작</LandingButton>
+      </Container>
+    </main>
+  );
 }
---
+++ src/components/home/home.styles.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { Box, Button } from "@mui/material";
+
+export const Container = styled(Box)`
+  width: 300px;
+  height: 300px;
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid #222;
+`;
+
+export const Title = styled(Box)``;
+
+export const LandingButton = styled(Button)``;

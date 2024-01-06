e6f0aba469560416068348f5551f38d4eec21b1f
youznn <leejin914418@gmail.com>
2024-01-07 02:43:08 +0900

    set font and etc

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,17 @@ import {
   Title,
   LandingButton,
   Container,
-  CopyUriBox,
-  CopyUriButton,
-  CopyUri,
   InfoBox,
   InfoImage,
   InfoText,
+  NameField,
+  BackgroundContainer,
 } from "@/components/home/home.styles";
-import { TextField } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import Image from "next/image";
 import infoImage from "@/assets/info_img.png";
+import backgroundImg from "@/assets/background_img.png";
 
 const BASE_URL = process.env.NEXT_PUBLIC_DEV_URL;
 
@@ -62,48 +61,56 @@ export default function Home() {
             <Image
               src={infoImage}
               alt="info_img"
-              width={370}
-              height={320}
+              width={250}
+              height={200}
             ></Image>
             <p>정답: 저염된장으로 맛을 낸 황태해장국</p>
           </InfoImage>
           <InfoText>
             <p>게임 방법</p>
-            1. 120초 안에 마성의 요리명을 맞혀 주세요! 정답을 맞히면 100점을
-            획득하고 다음 문제로 넘어갑니다. <br />
-            2. 40초, 80초가 경과해도 정답자가 없다면 힌트가 1개씩 노출됩니다.
-            <br />
-            3. 120초동안 아무도 정답자가 없다면 모두의 하트가 1개 감소합니다.{" "}
-            <br />
-            4. 최초로 주어진 하트 3개가 모두 없어지면 게임이 오버되니
-            주의하세요! <br />
-            5. 나가기 버튼을 누르면 게임에서 나갈 것인지 재확인하며 역대
-            10위까지의 점수가 나타납니다. 명예의 전당에 자신의 기록을 등록해
-            보세요!
-            <br />
-            <br />
-            ※주의: 이 게임은 실시간 게임으로, 최초 입장 시 남은 시간이 120초
-            미만일 수 있습니다.
+            <ol>
+              <li>
+                120초 안에 마성의 요리명을 맞혀 주세요! 정답을 맞히면 100점을
+                획득하고 다음 문제로 넘어갑니다.
+              </li>
+              <li>
+                40초, 80초가 경과해도 정답자가 없다면 힌트가 1개씩 노출됩니다.
+              </li>
+              <li>
+                120초동안 아무도 정답자가 없다면 모두의 하트가 1개 감소합니다.
+              </li>
+              <li>
+                최초로 주어진 하트 3개가 모두 없어지면 게임이 오버되니
+                주의하세요!
+              </li>
+              <li>
+                나가기 버튼을 누르면 게임에서 나갈 것인지 재확인하며 역대
+                10위까지의 점수가 나타납니다. 명예의 전당에 자신의 기록을 등록해
+                보세요!
+              </li>
+            </ol>
+            <p1>
+              ※주의: 이 게임은 실시간 게임으로, 최초 입장 시 남은 시간이 120초
+              미만일 수 있습니다.
+            </p1>
+
+            <div>
+              <NameField
+                placeholder="이름을 입력하세요"
+                onChange={handleSetName}
+              />
+              <LandingButton onClick={handleStartGame}>게임 시작</LandingButton>
+            </div>
           </InfoText>
         </InfoBox>
-        <TextField placeholder="이름을 입력하세요" onChange={handleSetName} />
-        <LandingButton variant="contained" onClick={handleStartGame}>
-          게임 시작
-        </LandingButton>
-
-        <CopyUriBox>
-          <p>친구 초대하기</p>
-          <div>
-            <CopyUri> http어쩌고</CopyUri>
-            <CopyUriButton
-              variant="contained"
-              onClick={() => handleCopyClipBoard()}
-            >
-              Copy
-            </CopyUriButton>
-          </div>
-        </CopyUriBox>
       </Container>
+      <Image
+        src={backgroundImg}
+        alt="background_img"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
     </main>
   );
 }
